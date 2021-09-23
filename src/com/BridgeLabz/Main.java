72b81cf3f2ea8790c2f6dd683bbb16c02fda4584
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-24 01:09:27 +0530

    Adding the switch statement to compute employee wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -1,26 +1,26 @@
 package com.BridgeLabz;
 import java.util.Random;
 public class Main {
-
+    static final int IS_FULL_TIME=1;
+    static final int IS_PART_TIME=2;
+    static final int EMP_RATE_PerHr=20;
     public static void main(String[] args) {
         System.out.println("Welcome to the employeewage compute");
-        // declare the constant in the program
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int EMP_RATE_PerHr=20;
         //declare a variable to compute
         int empHrs=0, empWage=0;
         // random to compute the value of random number rang of 2 number
         Random random = new Random();
         int empCheck = random.nextInt(3);
-        if(empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-        }
-        else if(empCheck == IS_PART_TIME){
-            empHrs = 4;
-        }
-        else{
-            empHrs=0;
+        switch(empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+                break;
         }
         // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
         empWage = EMP_RATE_PerHr * empHrs;
