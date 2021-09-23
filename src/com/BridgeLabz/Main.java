6125512321e675d42f349319a81ebce762abbdb2
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-24 01:16:29 +0530

    Adding the switch statement to compute employee wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -4,26 +4,29 @@ public class Main {
     static final int IS_FULL_TIME=1;
     static final int IS_PART_TIME=2;
     static final int EMP_RATE_PerHr=20;
+    static final int NO_OF_WORKINGDAY = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to the employeewage compute");
         //declare a variable to compute
-        int empHrs=0, empWage=0;
+        int empHrs = 0, empWage = 0;
         // random to compute the value of random number rang of 2 number
-        Random random = new Random();
-        int empCheck = random.nextInt(3);
-        switch(empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
-                break;
+        for (int i = 0; i <= NO_OF_WORKINGDAY; i++) {
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
+            empWage = EMP_RATE_PerHr * empHrs;
+            System.out.println("Daily wage of the employee" + empWage);
         }
-        // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
-        empWage = EMP_RATE_PerHr * empHrs;
-        System.out.println("Daily wage of the employee"+empWage);
     }
 }
