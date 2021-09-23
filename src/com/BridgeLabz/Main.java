94241e428542b0668e3c03b19d0e1e9d63fee3b8
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-24 02:06:45 +0530

    Added while loop to compute emp wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -5,19 +5,20 @@ public class Main {
     static final int IS_PART_TIME=2;
     static final int EMP_RATE_PerHr=20;
     static final int NO_OF_WORKINGDAY = 20;
+    static final int MAX_HRS_MONTH=100;
     public static void main(String[] args) {
         System.out.println("Welcome to the employeewage compute");
         //declare a variable to compute
-        int empHrs = 0, empWage = 0;
+        int empHrs = 0, empWage = 0,totalEmpWage=0,totalEmpHr=0,totalWorkingDays=0;
         // random to compute the value of random number rang of 2 number
-        for (int i = 0; i <= NO_OF_WORKINGDAY; i++) {
+        while(totalEmpHr<=MAX_HRS_MONTH && totalWorkingDays<NO_OF_WORKINGDAY){
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
-                case IS_FULL_TIME:
+                case IS_FULL_TIME:    //full time employee
                     empHrs = 8;
                     break;
-                case IS_PART_TIME:
+                case IS_PART_TIME:    //part time employee
                     empHrs = 4;
                     break;
                 default:
@@ -26,7 +27,10 @@ public class Main {
             }
             // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
             empWage = EMP_RATE_PerHr * empHrs;
-            System.out.println("Daily wage of the employee" + empWage);
+            totalEmpHr += empHrs;
+            System.out.println("working day"+totalWorkingDays+"employee work"+empHrs+"hour employee wage"+empWage);
         }
+        totalEmpWage = totalEmpHr * EMP_RATE_PerHr;
+        System.out.println("Month employee wage"+totalEmpWage);
     }
 }
