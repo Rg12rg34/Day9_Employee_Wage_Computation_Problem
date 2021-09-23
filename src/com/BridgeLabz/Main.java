c3375d714fff19e5f5083b24fe4fb0d565bce82a
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-24 01:03:27 +0530

    Adding the part time employee hr and compute the employee wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -6,17 +6,21 @@ public class Main {
         System.out.println("Welcome to the employeewage compute");
         // declare the constant in the program
         int IS_FULL_TIME=1;
+        int IS_PART_TIME=2;
         int EMP_RATE_PerHr=20;
         //declare a variable to compute
         int empHrs=0, empWage=0;
         // random to compute the value of random number rang of 2 number
         Random random = new Random();
-        int empCheck = random.nextInt(2);
+        int empCheck = random.nextInt(3);
         if(empCheck == IS_FULL_TIME) {
             empHrs = 8;
         }
-        else{
+        else if(empCheck == IS_PART_TIME){
             empHrs = 4;
+        }
+        else{
+            empHrs=0;
         }
         // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
         empWage = EMP_RATE_PerHr * empHrs;
