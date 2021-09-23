dc9f11c2db7bf87e0db3a5e235da248007d15f9d
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-24 00:52:46 +0530

    Adding the emp rate per hr to compute the dail wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -6,14 +6,20 @@ public class Main {
         System.out.println("Welcome to the employeewage compute");
         // declare the constant in the program
         int IS_FULL_TIME=1;
+        int EMP_RATE_PerHr=20;
+        //declare a variable to compute
+        int empHrs=0, empWage=0;
         // random to compute the value of random number rang of 2 number
         Random random = new Random();
         int empCheck = random.nextInt(2);
         if(empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is present");
+            empHrs = 8;
         }
         else{
-            System.out.println("Employee is absent");
+            empHrs = 4;
         }
+        // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
+        empWage = EMP_RATE_PerHr * empHrs;
+        System.out.println("Daily wage of the employee"+empWage);
     }
 }
