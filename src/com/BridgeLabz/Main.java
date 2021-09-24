39151a4582344607bb99e8829a1810b4e89c2edd
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-24 22:09:14 +0530

    Added method to compute emp wage

--- src/com/BridgeLabz/Main.java
+++ src/com/BridgeLabz/Main.java
@@ -1,17 +1,20 @@
 package com.BridgeLabz;
 import java.util.Random;
+class employee{
+}
 public class Main {
     static final int IS_FULL_TIME=1;
     static final int IS_PART_TIME=2;
     static final int EMP_RATE_PerHr=20;
     static final int NO_OF_WORKINGDAY = 20;
     static final int MAX_HRS_MONTH=100;
-    public static void main(String[] args) {
-        System.out.println("Welcome to the employeewage compute");
-        //declare a variable to compute
-        int empHrs = 0, empWage = 0,totalEmpWage=0,totalEmpHr=0,totalWorkingDays=0;
+    /* employeeWage method to compute the employee wage
+     * employee wage compute with the switch case and random to give the working day of employee
+     */
+    public static void employeeWage() {
+        int empHrs = 0, empWage = 0, totalEmpWage = 0, totalEmpHr = 0, totalWorkingDays = 0;
         // random to compute the value of random number rang of 2 number
-        while(totalEmpHr<=MAX_HRS_MONTH && totalWorkingDays<NO_OF_WORKINGDAY){
+        while (totalEmpHr <= MAX_HRS_MONTH && totalWorkingDays < NO_OF_WORKINGDAY) {
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
@@ -28,9 +31,14 @@ public class Main {
             // daily wage give the employee rate per hour 20 and the emp hour 8 of day compute
             empWage = EMP_RATE_PerHr * empHrs;
             totalEmpHr += empHrs;
-            System.out.println("working day"+totalWorkingDays+"employee work"+empHrs+"hour employee wage"+empWage);
+            System.out.println("working day" + totalWorkingDays + "employee work" + empHrs + "hour employee wage" + empWage);
         }
         totalEmpWage = totalEmpHr * EMP_RATE_PerHr;
         System.out.println("Month employee wage"+totalEmpWage);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employeewage compute");
+        // call the employeeWage method
+        employeeWage();
     }
 }
